ca98c60b0fb0882fb2d61457d59c88b38f92261d
Michael Henderson <m.nishimoto97@gmail.com>
2025-08-26 15:00:22 +0900

    Added the scan view and controller (new + create), as well as javascript to preview photo

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -7,5 +7,18 @@ class ScansController < ApplicationController
   end
 
   def create
+    @scan = current_user.scans.new(scan_params)
+
+    if @scan.save
+      redirect_to scan_path, notice: "Photo uploaded successfully!"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def scan_params
+    params.require(:scan).permit(:photo)
   end
 end
