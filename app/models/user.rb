5ddc7b7f39fbac596e8188e4aa4e4c91c7f83550
Rafael Pradillo <tasfa66@gmail.com>
2025-08-26 18:07:00 +0900

    un validated the username since we dont ask for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :scans
   has_many :recipes, through: :scans
-  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
