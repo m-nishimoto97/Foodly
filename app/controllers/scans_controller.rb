27e275a1d881ac061328f0cb71f4b58c78f9c684
Michael Henderson <m.nishimoto97@gmail.com>
2025-08-27 17:53:57 +0900

    Fixed the scans#create so that I can properly get the image

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -9,24 +9,25 @@ class ScansController < ApplicationController
   def create
     @scan = current_user.scans.new(scan_params)
 
-    chat = RubyLLM.chat
-    prompt = <<-PROMPT
-      Analyze the image and return ONLY an array of ingredients that clearly appear in the photo.
-      The output must be a valid JSON array of strings, e.g. ["pork", "onions", "carrots"].
-
-      Rules:
-      - Do NOT use vague categories like "meat", "vegetables", or "fruit".
-      - Instead, use the most specific general name possible (e.g. "pork", "chicken", "apple", "cucumber").
-      - If you cannot confidently identify an ingredient, DO NOT include it in the array.
-      - Do not include brand names.
-      - Do not add explanations or extra text — output the array only.
-    PROMPT
-    response = chat.ask(prompt, with: { image: @scan.photo })
-    ingredients = JSON.parse(response.content)
-    @scan.ingredients = ingredients
-
     if @scan.save
-      redirect_to new_scan_recipe_path, notice: "Photo uploaded successfully!"
+      chat = RubyLLM.chat
+      prompt = <<-PROMPT
+        Analyze the image and return ONLY an array of ingredients that clearly appear in the photo.
+        The output must be a valid JSON array of strings, e.g. ["pork", "onions", "carrots"].
+
+        Rules:
+        - Do NOT use vague categories like "meat", "vegetables", or "fruit".
+        - Instead, use the most specific general name possible (e.g. "pork", "chicken", "apple", "cucumber").
+        - If you cannot confidently identify an ingredient, DO NOT include it in the array.
+        - Do not include brand names.
+        - Do not add explanations or extra text — output the array only.
+      PROMPT
+
+      response = chat.ask(prompt, with: { image: @scan.photo })
+      ingredients = JSON.parse(response.content)
+      @scan.ingredients = ingredients
+
+      redirect_to new_scan_recipe_path(@scan), notice: "Photo uploaded successfully!"
     else
       render :new, status: :unprocessable_content
     end
