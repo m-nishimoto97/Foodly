2c2199914c6ee802fa48c20311586fdb7ae55dcb
Michael Henderson <m.nishimoto97@gmail.com>
2025-08-27 12:14:49 +0900

    Fixed photo preview

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,13 @@ class RecipesController < ApplicationController
   end
 
   def create
-
+    ingredients = ["pork", "onions", "tofu", "sesame oil", "gochujang", "gochugaru", "garlic", "eggs"]
+    time = 15
+    prompt = <<-PROMPT
+      Generate a recipe using only #{ingredients.join(',')} that take only #{time} minutes.
+      Include the recipe's name, duration, diet (if present such as vegetarian or vegan), cuisine, and directions.
+      Include it in json file
+    PROMPT
   end
 
   def show
