34386d7165c44b9f3bd1cd53bc242ccd27507487
kaleb fukuda <kalebfukudajp@gmail.com>
2025-09-03 09:33:37 +0900

    Corrected find recipe function, added highlight to selected recipe

--- app/javascript/controllers/scheduler_controller.js
+++ app/javascript/controllers/scheduler_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="scheduler"
 export default class extends Controller {
-  static targets = ["searchinput"]
+  static targets = ["searchinput", "recipeInput", "divRecipeCards"]
   static values = { url: String }
 
   submit() {
@@ -12,6 +12,15 @@ export default class extends Controller {
     })
     .then(response => response.text())
     .then(html => Turbo.renderStreamMessage(html))
+  }
+
+  selectRecipe(event) {
+    const cards = this.divRecipeCardsTarget.querySelectorAll(".card-recipe");
+    cards.forEach(element => {
+      element.classList.remove("card-highlight");
+    });
+    event.currentTarget.classList.add("card-highlight");
+    this.recipeInputTarget.value = event.currentTarget.dataset.id;
   }
 
 }
